248f1173fdcf068af2a731d828e54e7bc58e72bc
David19 <davidleonel911@gmail.com>
2026-04-27 20:52:36 -0500

    .

--- Logica/DiccObj.cs
+++ Logica/DiccObj.cs
@@ -17,3 +17,16 @@ foreach (var par in diccionario)
 {
     Console.WriteLine($"llave {par.Key} el profesor es {par.Value.Anio}");
 }
+
+
+
+/*
+namespace DiccionarioDeObjetos.Logica;
+
+public class Materias
+{
+    public string Nombre{get; set;}
+    public int Anio{get; set;}
+    public string Profesor{get; set;}
+}
+*/
--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
-﻿namespace DiccionarioDeObjetos.Logica;
-
-public class Materias
-{
-    public string Nombre{get; set;}
-    public int Anio{get; set;}
-    public string Profesor{get; set;}
-}
+﻿
