9cbd9256c42ab435409cb205d12b8fb92c5897c4
David19 <davidleonel911@gmail.com>
2026-05-10 20:12:43 -0500

    Notificaciones

--- POO/Polimorfismo/Notificaciones.cs
+++ POO/Polimorfismo/Notificaciones.cs
@@ -1,23 +1,3 @@
-/*
-El Reto: Sistema de Notificaciones Polimórfico: Vas a crear un sistema que envíe alertas a los usuarios de distintas formas, usando la misma orden de ejecución pero comportamientos diferentes (virtual, override y sealed).
-
-Requerimientos:
-1. Clase Base: NotificadorBase
-Propiedad: Destinatario (tipo string, de solo lectura al inicializar con init).
-Constructor: Debe recibir y asignar el destinatario.
-Método: Crea un método public virtual void Enviar(string mensaje). Debe imprimir en consola: "[Sistema] Mensaje estándar para {Destinatario}: {mensaje}".
-
-2. Clase Hija 1: NotificadorEmail
-Hereda de NotificadorBase.
-Constructor: Exige el destinatario y se lo envía al constructor base.
-Método: Sobrescribe (override) el método Enviar. Debe imprimir: "[Email] Conectando al puerto 587. Enviando a {Destinatario}: {mensaje}".
-
-3. Clase Hija 2: NotificadorPush
-Hereda de NotificadorBase.
-Constructor: Exige el destinatario y se lo envía al constructor base.
-Método: Sobrescribe el método Enviar y séllalo (sealed) para que ninguna futura clase que herede de NotificadorPush pueda cambiar la forma en que se envían los mensajes Push. Debe imprimir: "[FCM Push] Despertando dispositivo de {Destinatario}. Payload: {mensaje}".
-*/
-
 //override: indica que vas a sobreescribir un metodo. el padre o metodo que estes modificando debe tener virtual
 //virtual: da permiso para que las hijas sobreescriban
 //sealed: indica que ya no puedes sobreescribir las que herenden, puede ser usado en una clase o metodo 
