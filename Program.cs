3b595c28412d3787999e5550d87e72fba395a5d2
David19 <davidleonel911@gmail.com>
2026-05-20 20:20:27 -0500

    datos genericos

--- Program.cs
+++ Program.cs
@@ -1,14 +1,1 @@
-﻿using Genericos.POO;
-
-
-Caja<string> caja1 = new Caja<string>("mensaje de string");
-
-
-Caja<bool> caja2 = new Caja<bool>(true);
-
-
-Caja<int> caja3 = new Caja<int>(99);
-
-caja1.MostrarContenido();
-caja2.MostrarContenido();
-caja3.MostrarContenido();
+﻿
