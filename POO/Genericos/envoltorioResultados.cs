95e7b9e54d77215c3b203cd8be3b283a0622463b
David19 <davidleonel911@gmail.com>
2026-05-23 20:11:26 -0500

    Resultados con datos genericos

--- POO/Genericos/envoltorioResultados.cs
+++ POO/Genericos/envoltorioResultados.cs
@@ -17,3 +17,12 @@ public class ResultadoOperacion<T>
         Console.WriteLine($"[Éxito: {Estado}] Mensaje: {Mensaje} | Datos: {Datos}");
     }
 }
+
+/*
+using envoltorioDeResultados.POO;
+
+ResultadoOperacion<int> resultado1 = new ResultadoOperacion<int>(true, "Usuario encontrado", 1050);
+resultado1.MostrarResumen();
+ResultadoOperacion<string> resultado2 = new ResultadoOperacion<string>(true, "Token Generado", "ABC-123-XYZ");
+resultado2.MostrarResumen();
+*/
