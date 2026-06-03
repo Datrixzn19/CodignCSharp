fa818f8686008b441a9a9369b5d90cf361aeda6c
David19 <davidleonel911@gmail.com>
2026-06-03 18:34:05 -0500

    logica para el control de peso y lanzamiento de excepciones

--- POO/ExepcionesPOO/Ejercicio2/Bascula.cs
+++ POO/ExepcionesPOO/Ejercicio2/Bascula.cs
@@ -10,3 +10,27 @@ Si el peso > 50, lanza SobrepesoException con el mensaje: "Paquete rechazado: El
 Si todo está bien, imprime en consola: "Paquete de {peso} kg procesado correctamente para su envío."
 
 */
+
+
+
+namespace excLogistica.POO;
+
+public class Bascula
+{
+    public void ProcesaPaquete(double peso)
+    {
+        if(peso <= 0)
+        {
+            throw new PesoInvalidoException("Error de lectura: el peso no puede ser cero o negativo");
+        }
+        else if(peso >+ 50)
+        {
+            throw new SobrepesoException("Paquete rechazado: El límite máximo es de 50 kg");
+        }
+        else
+        {
+            Console.WriteLine("Paquete de {peso} kg procesado correctamente para su envío.");
+        }
+
+    }
+}
