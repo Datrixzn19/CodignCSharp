aabde970e35002003f55997468f29fe1d9197c69
David19 <davidleonel911@gmail.com>
2026-06-03 18:43:31 -0500

    Exepciones con POO Controld e peso

--- POO/ExepcionesPOO/Ejercicio2/ExcepcionesEnvio.cs
+++ POO/ExepcionesPOO/Ejercicio2/ExcepcionesEnvio.cs
@@ -10,3 +10,27 @@ public class SobrepesoException : Exception//Para cuando se exeda el peso permit
 {
     public SobrepesoException(string msj) : base(msj){}
 }
+
+/*
+using excLogistica.POO;
+
+
+Bascula basc = new Bascula();
+
+
+try
+{
+    //basc.ProcesaPaquete(50);
+    basc.ProcesaPaquete(49.9);
+    basc.ProcesaPaquete(0);
+}
+catch (PesoInvalidoException ex)
+{
+    
+    Console.WriteLine($"ERROR {ex.Message}");
+}
+catch(SobrepesoException ex)
+{
+    Console.WriteLine($"ERROR {ex.Message}");
+}
+*/
