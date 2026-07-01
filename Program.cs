d426f54a1395bc5e17a7fcc5895e8254d0c39099
David19 <davidleonel911@gmail.com>
2026-07-01 10:58:14 -0500

    where en linq

--- Program.cs
+++ Program.cs
@@ -2,33 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 
+    
 
-List<Producto> inventario = new List<Producto>
+
+// BASE DE DATOS SIMULADA
+
+List<VideojuegoLinq> catalogo = new List<VideojuegoLinq>
 {
-    new Producto { Nombre = "Laptop", Precio = 1200, Categoria = "Electrónica" },
-    new Producto { Nombre = "Ratón", Precio = 25, Categoria = "Electrónica" },
-    new Producto { Nombre = "Escritorio", Precio = 300, Categoria = "Muebles" },
-    new Producto { Nombre = "Silla", Precio = 150, Categoria = "Muebles" }
+    new VideojuegoLinq { Titulo = "Zelda: Breath of the Wild", Anio = 2017, Calificacion = 9.8, EsMultijugador = false },
+    new VideojuegoLinq { Titulo = "Minecraft", Anio = 2011, Calificacion = 9.5, EsMultijugador = true },
+    new VideojuegoLinq { Titulo = "Cyberpunk 2077", Anio = 2020, Calificacion = 7.5, EsMultijugador = false },
+    new VideojuegoLinq { Titulo = "Mario Kart 8", Anio = 2014, Calificacion = 8.9, EsMultijugador = true },
+    new VideojuegoLinq { Titulo = "Tetris", Anio = 1984, Calificacion = 9.0, EsMultijugador = false }
 };
 
 
-/*
-// OrderBy: Ordena la colección 
-// La lambda 'p => p.Precio' indica qué propiedad se usará como criterio de ordenación (ascendente por defecto).
-List<Producto> inventarioOrdenado = inventario.OrderBy(p => p.Precio).ToList();
+//RETOS LINQ
 
-Console.WriteLine("\n2. ORDENADOS POR PRECIO:");
-foreach (var p in inventarioOrdenado) 
+
+// Guardar en una lista los juegos con Calificacion MAYOR a 8.5
+
+List<VideojuegoLinq> mayorCalificaciones = catalogo.Where(p => p.Calificacion > 8.5).ToList();
+Console.WriteLine("Mejores calificaciones de videojuegos");
+foreach (var item in mayorCalificaciones)
 {
-    Console.WriteLine($"- {p.Nombre} (${p.Precio})");
+    Console.WriteLine($"{item.Titulo} {item.Calificacion}");
 }
 
-*/
 
 
-public class Producto
+
+
+
+
+
+
+
+
+
+
+
+// Reto 2: Usa .OrderBy() para ordenar los juegos por 'Anio' (del más viejo al más nuevo)
+// Escribe tu código aquí...
+
+// Reto 3: Usa .FirstOrDefault() para buscar el juego que se llame exactamente "Minecraft"
+// Escribe tu código aquí...
+
+// Reto 4 (Combinado): Usa .Where() para buscar los juegos multijugador y luego .Select() para extraer solo sus Títulos.
+// Escribe tu código aquí...
+
+// Reto 5 (Bonus): Adivina o busca cómo usar el método .Count() para saber la cantidad exacta (int) de juegos multijugador.
+// Escribe tu código aquí...
+
+
+
+// DECLARACIÓN DE CLASE
+
+public class VideojuegoLinq
 {
-    public string Nombre { get; set; }
-    public int Precio { get; set; }
-    public string Categoria { get; set; }
+    public string Titulo { get; set; }
+    public int Anio { get; set; }
+    public double Calificacion { get; set; }
+    public bool EsMultijugador { get; set; }
 }
+
+
